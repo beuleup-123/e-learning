64fa289fc11b7b719a03505a8c66fdbad8cb380e
Birame <beuleup3951@gmail.com>
2020-01-11 10:12:44 +0000

    correct error appProv

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -34,7 +34,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         if (config('app.env')==='production') {
-            \URL::forceScheme('https');
+            \URL::forceSchema('https');
         }
         \Illuminate\Support\Facades\Schema::defaultStringLength(191);
     }
